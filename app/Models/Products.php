11f7485d3922e9fcde8753fa62ec1499e18b9156
halick <mekde@hotmail.fr>
2024-07-08 17:44:23 +0200

    Add random user

--- app/Models/Products.php
+++ app/Models/Products.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Products extends Model
 {
     use HasFactory;
+    protected $fillable = ['category_id','name','description','price','images',];
+    /**
+     * Get the category that owns the Products
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
 }
